0c753ff1808586c46a92321e14f2dc5e088ad294
Lucas Pereira <lucaspereirasantanadesouza@hotmail.com>
2026-06-18 09:06:23 -0300

    create Seller

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import db.DB;
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
 
+        Connection conn = DB.getConnection();
+
+        if (conn != null) {
+            System.out.println("Ligação à base de dados realizada com sucesso!");
+        }
+
+        DB.closeConnection();
     }
 }
